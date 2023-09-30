bdc8bc62b29cf7b591736d8e212127b1632d1c71
annako-io <annako1@icloud.com>
2023-09-29 22:12:26 -0700

    completed follow system on user and follow models, and tested on the console

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,5 +24,13 @@ class User < ApplicationRecord
   # has_many :waiting_sent_requests, -> {where(accepted: false)}, class_name: "Follow", foreign_key: "follower_id"
 
   has_many :followers, through: :accepted_received_requests, source: :follower
-  has_many :following, through: :accepted_sent_requests, source: :followed
+  has_many :followings, through: :accepted_sent_requests, source: :followed
+
+  def follow(user)
+    Follow.create(follower: self, followed: user)
+  end
+
+  def unfollow(user)
+    self.accepted_sent_request.find_by(followed: user)&.destroy
+  end
 end
